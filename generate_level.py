bfe58bbeadb9a08631cc08d5c24d091c4c18c0c8
Henry Baldwin <henrymbaldwin@gmail.com>
2023-09-13 15:55:13 -0400

    Created generate_level.py, currently untested but theoretically capable of generating a valid but not necassarily solvable level in the form of a grid array.

---
+++ generate_level.py
@@ -0,0 +1,167 @@
+
+import random
+
+#random level generator - not garunteed solvable
+#also generates metadata on each level in json to later tune the generator to more reliably produce solvable levels
+#based purely on encoded form of the grid
+class Generator:
+
+	def __init__():
+		#empty grid
+		self.grid = [[".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."]]
+
+		self.grid_data = {
+			"grid":[],
+			"num_blocks":0,
+			"red_block_col":-1,
+			"vert_short":0,
+			"vert_long":0,
+			"hori_short":0,
+			"hori_long":0
+			"moves_to_solve":0
+			"solvable":false
+		}
+
+		self.block_info{
+			"A":"hori_short"
+			"a":"vert_short"
+			"B":"hori_long"
+			"b":"vert_long"
+		}
+
+
+	def reset():
+
+		self.grid = [[".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."],
+		 [".", ".", ".", ".",".","."]]
+
+		self.grid_data = {
+			"grid":[],
+			"num_blocks":0,
+			"red_block_col":-1,
+			"vert_short":0,
+			"vert_long":0,
+			"hori_short":0,
+			"hori_long":0
+			"moves_to_solve":0
+			"solvable":false
+		}
+
+		
+	#handles block placement - returns true if block is placed successfully
+	def place_block(block, r, c):
+
+		grid = self.grid
+		ret = False
+		if r < 0 or c < 0 or r > 5 or c >5:
+			return False
+
+		match block:
+			case "R":
+				if r != 3 or c > 3:
+					break
+				#move straight to "A" code to avoid repition 
+			case "A":
+				if grid[r][c] != "." or grid[r][c+1] != "." or c == 5
+					break
+				grid[r][c] = block
+				grid[r][c+1] =  block
+				ret = True
+				break
+			case "a":
+				if grid[r][c] != "." or grid[r+1][c] != "." or r == 5
+					break
+				grid[r][c] = block
+				grid[r+1][c] =  block
+				ret = True
+				break
+			case "B":
+				if grid[r][c] != "." or grid[r][c+1] != "." or grid[r][c+2] != "." or c > 3
+					break
+				grid[r][c] = block
+				grid[r][c+1] =  block
+				grid[r][c+2] =  block
+				ret = True
+				break
+
+			case "b":
+				if grid[r][c] != "." or grid[r+1][c] != "." or grid[r+2][c] != "." or r > 3
+					break
+				grid[r][c] = block
+				grid[r+1][c] =  block
+				grid[r+2][c] =  block
+				ret = True
+				break
+
+		self.grid = grid
+		return ret
+
+	def print_state(self):
+		#prints a game state for debugging purposes		
+		for row in grid:
+				line = "[ "
+				for cell in row:
+					line += cell + " "
+				line += "]"
+
+	#generates random level
+	def generate_level()
+
+		#reset
+		reset()
+
+		#place red block
+
+		red_block_col = random.randint(0,3)
+		place_block("R", 3, red)
+
+		self.grid_data["red_block_col"] = red_block_col
+		#choose number of brown blocks between 4 and 13
+		#this number range is largely arbitrary based on a cursory examination of a few unblock me levels
+		num_blocks = random.randint(4,13)
+
+		for i in range(0, num_blocks+1):
+			#pick block, currently weighted 50/50 short/long and vertical/horizantal, meaning a quarter chance for every block type
+			#these may be altered in the future to more consistantly produce solvable levels based on statistics harvested from generated levels
+			block_type_int = randint(0,3)
+
+			block = ""
+			switch block_type_int:
+				case 0:
+					block = "A"
+					break
+				case 1:
+					block = "a"
+					break
+				case 2:
+					block = "B"
+					break
+				case 3: 
+					block = "b"
+					break
+
+			#try to place block at random coordinates 1000 times, if block cannot be placed, move on
+			#this is a very inefficient way to do this, oh well, if its significantly delaying the program I'll change it I guess
+			for i in range(1000):
+				#pick coordinates
+				r = randint(0,5)
+				c = randint(0,5)
+
+				if place_block(block, r, c):
+					#if block was succesfully placed add it to meta data
+					self.grid_data["num_blocks"] += 1
+					self.grid_data[block_info[block]] += 1
+					break
+
+		#add grid to metadata
+
+		self.grid_data["grid"] = self.grid
