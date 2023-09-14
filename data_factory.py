4fa195739585f7e4de270d3dd4deef5de7c5b045
Henry Baldwin <henrymbaldwin@gmail.com>
2023-09-14 12:14:23 -0400

    Continued working on generate_levels with the unfortunate realization that my naming schemes have clearly fallen apart.

--- data_factory.py
+++ data_factory.py
@@ -13,4 +13,11 @@ class Level_Factory:
 	#if !solvable then this will log both solvable
 	def generate_levels(self, num, solvable = True):
 
-		for i in range()
+		for i in range(num):
+			grid_data = generator.generate_level()
+			grid = grid_data["grid"]
+
+			#this line represents a failure in so  many ways at a very high level. TBF I wrote the initial solver years before I decide to write the rest of this.
+			#I could refactor this pretty easily, but I wont.
+			#Don't smooth moves for clean data
+			solution = unblocker.unblock(board = grid, grid = None, smooth = False)
