a47db025b86be1a0983bf32d6e23f045f697f994
Henry Baldwin <henrymbaldwin@gmail.com>
2023-09-24 18:15:14 -0400

    Generated 1000 levels linearly, added storage of seen game states to ensure no duplicate solve attempts or duplicate solve data

--- data_factory.py
+++ data_factory.py
@@ -1,54 +1,73 @@
 import json
 import os
+import time
 import generate_level
 import unblock
 
-#Produces solvable levels with metadata including solution
 class Level_Factory:
 
-	def __init__(self):
-		self.generator = generate_level.Generator()
-		self.unblocker = unblock.Unblocker()
+    def __init__(self):
+        self.generator = generate_level.Generator()
+        self.unblocker = unblock.Unblocker()
+        self.unique_states_file = "levels/unique_game_states.txt"  # Set the file path to the levels folder
+        self.unique_game_states = self.load_unique_game_states()  # Initialize the set by loading from the file
 
-	#generates and stores num levels, by default these are the number of solvable levels to generate
-	#if !solvable then this will log both solvable
-	def generate_levels(self, num, solvable = True):
+    def load_unique_game_states(self):
+        unique_states = set()
+        if os.path.exists(self.unique_states_file):
+            with open(self.unique_states_file, "r") as file:
+                for line in file:
+                    unique_states.add(line.strip())
+        return unique_states
 
-		for i in range(num):
-			while(True):
-				grid_data = self.generator.generate_level()
-				grid = grid_data["grid"]
-				#this line represents a failure in so  many ways at a very high level. TBF I wrote the initial solver years before I decide to write the rest of this.
-				#I could refactor this pretty easily, but I wont.
-				#Don't smooth moves for clean data
-				solution = self.unblocker.solve_board(board = grid, smooth = False)
+    def save_unique_game_states(self):
+        with open(self.unique_states_file, "w") as states_file:
+            for state_str in self.unique_game_states:
+                states_file.write(state_str + "\n")
 
-				if len(solution) != 0 and solvable:
-					print("Level " + str(i))
-					for move in solution:
-						print(move)
-					break
-				elif not solvable:
-					break
-	
-	#saves a level to level_data.json
-	def save_level(level_data, filename="levels/level_data.json"):
-	    # Check if the JSON file already exists
-	    if os.path.exists(filename):
-	        # Load existing data from the file
-	        with open(filename, "r") as file:
-	            existing_data = json.load(file)
-	    else:
-	        # Create a new data structure if the file doesn't exist
-	        existing_data = []
+    def generate_levels(self, num, solvable=True):
+        for i in range(num):
+            while True:
+                grid_data = self.generator.generate_level()
+                grid = grid_data["grid"]
+                state_str = ''.join(''.join(row) for row in grid)
 
-	    # Append the new level data to the existing data
-	    existing_data.append(level_data)
+                if state_str not in self.unique_game_states:
+                    solution = self.unblocker.solve_board(board=grid, smooth=False)
 
-	    # Write the updated data back to the JSON file
-	    with open(filename, "w") as file:
-	        json.dump(existing_data, file, indent=4)
+                    grid_data["solution"] = solution
+                    if len(solution) != 0 and solvable:
+                        print("Level " + str(i))
+                        for move in solution:
+                            print(move)
+
+                        grid_data["solvable"] = True
+                        grid_data["moves_to_solve"] = len(solution)
+                        self.save_level(grid_data)
+
+                    # Add to the set and save if unique
+                    self.unique_game_states.add(state_str)
+                    self.save_unique_game_states()
+                    break
+                elif not solvable:
+                    break
+
+    def save_level(self, level_data, filename="levels/level_data.json"):
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                existing_data = json.load(file)
+        else:
+            existing_data = []
+
+        existing_data.append(level_data)
+
+        with open(filename, "w") as file:
+            json.dump(existing_data, file, indent=4)
 
 lfactory = Level_Factory()
 
-lfactory.generate_levels(100,True)
+st = time.time()
+lfactory.generate_levels(1000, True)
+et = time.time()
+
+print(str(et - st))
